0f4e1a7612ee62dff87217311b57b11f586d0e4a
nilsk <nils.kokeritz@gmail.com>
2021-11-17 14:15:56 +0000

    Fixed for firestore, fragment_user_profile.xml might be weird.

--- app/src/main/java/ie/ul/fika/Fragments/userProfile.java
+++ app/src/main/java/ie/ul/fika/Fragments/userProfile.java
@@ -1,4 +1,4 @@
-package ie.ul.fika;
+package ie.ul.fika.Fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import ie.ul.fika.R;
 
 /**
  * A simple {@link Fragment} subclass.
