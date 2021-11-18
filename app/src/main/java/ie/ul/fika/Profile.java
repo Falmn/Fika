7c5aaa5844f931553bf734c13d32953f18165da6
frejaboholm <frejaboholm@gmail.com>
2021-11-18 08:45:26 +0000

    Deleted firebase

--- app/src/main/java/ie/ul/fika/Profile.java
+++ app/src/main/java/ie/ul/fika/Profile.java
@@ -8,12 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Profile extends AppCompatActivity {
     TextView fullname, email, phone;
--- app/src/main/java/ie/ul/fika/SignUp.java
+++ app/src/main/java/ie/ul/fika/SignUp.java
@@ -23,12 +23,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+
 
 import java.util.HashMap;
 import java.util.Map;
